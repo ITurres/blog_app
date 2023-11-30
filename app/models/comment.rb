691166a82837b0dc489915bb686c4d3c5a48b98a
ITurres <emanueliturres.co@gmail.com>
2023-11-29 23:13:54 -0300

    Feat: Add Rails ActiveRecord callbacks to Like and Comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
   belongs_to :post, class_name: 'Post', foreign_key: :post_id
 
+  # * Rails ActiveRecord callbacks to update counters in User model after saving a Comment
+  after_save :update_comments_counter
+
   def update_comments_counter
     post.increment!(:comments_counter)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,9 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
   belongs_to :post, class_name: 'Post', foreign_key: :post_id
 
+  # * Rails ActiveRecord callbacks to update counters in User model after saving a Like
+  after_save :update_likes_counter
+
   def update_likes_counter
     post.increment!(:likes_counter)
   end

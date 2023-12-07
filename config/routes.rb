dff41173635a31cb77359a21f557ec335fe50ae0
ITurres <emanueliturres.co@gmail.com>
2023-12-07 19:18:36 -0300

    Feat: Add 'posts-new-create' routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new create]
   end
 end

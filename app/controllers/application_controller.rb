7eb422234078e8c1dd701c80fadbd11ed403fa6a
ITurres <emanueliturres.co@gmail.com>
2023-12-07 19:20:10 -0300

    Feat: Create 'ApplicationController#current_user'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    # @current_user ||= User.find_by(id: session[:user_id])
+    # ! fetch first user for testing purposes. Remove this line when login system in place.
+    @current_user = User.first
+  end
 end

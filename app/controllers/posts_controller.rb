a74f7211338a53bfc8114a5d5356fa6c08081f82
ITurres <emanueliturres.co@gmail.com>
2023-12-07 19:21:30 -0300

    Feat: Add controllers 'posts#new - posts#create - private posts#post_params'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,37 @@ class PostsController < ApplicationController
 
   def show
     p '------PostsController / show------'
+
     @post = Post.find(params[:id])
+  end
+
+  def new
+    p '------PostsController / new------'
+
+    @post = Post.new
+    @user = current_user
+  end
+
+  def create
+    p '------PostsController / create------'
+
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      flash[:success] = 'Post successfully created!'
+      redirect_to user_posts_path(current_user)
+    else
+      flash[:error] = 'Post could not be created, please try again...'
+      render :new, locals: { user: @user, post: @post }
+    end
+  end
+
+  private
+
+  def post_params
+    p '------PostsController / post_params------'
+
+    params.require(:post).permit(:title, :text)
   end
 end

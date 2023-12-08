aaac42dfd694bbb6ed96aa88fddea3fa3ee38fe7
ITurres <emanueliturres.co@gmail.com>
2023-12-07 21:35:39 -0300

    Add 'faker' data to 'seeds' for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# ? Create 10 random users.
+10.times do
+  user = User.create(
+    name: Faker::Name.unique.name,
+    photo: Faker::Internet.url,
+    bio: Faker::Lorem.sentence
+  )
+
+  # ? Create 5 random posts for each user.
+  5.times do
+    new_post = Post.create(
+      author: user,
+      title: Faker::Lorem.sentence,
+      text: Faker::Lorem.paragraph
+    )
+
+    # ? Create 3 random comments for each post.
+    6.times do
+      Comment.create(
+        post: new_post,
+        author: User.all.sample,
+        text: Faker::Lorem.sentence
+      )
+    end
+  end
+end

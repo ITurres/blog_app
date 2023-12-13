fe3b5ebb7336d87875455f91f9c6a107b622a5a7
ITurres <emanueliturres.co@gmail.com>
2023-12-13 15:18:01 -0300

    feat: Configure ApplicationController for Devise auth and param sanitization
    Implemented Devise authentication and parameter handling
    in ApplicationController:
    
    - Enforced user authentication across the app with
      `before_action :authenticate_user!`.
    - Protected against CSRF attacks with `protect_from_forgery
      with: :exception`.
    - Implemented strong parameters for user attributes in sign-up,
      updates, and sign-in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    # @current_user ||= User.find_by(id: session[:user_id])
-    # ! fetch first user for testing purposes. Remove this line when login system in place.
-    @current_user = User.first
+  before_action :authenticate_user!
+
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :bio, :password, :current_password)
+    end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email)
+    end
   end
 end

3f08e2556c6f2069ce8ccfaf490c933468505b1b
ITurres <emanueliturres.co@gmail.com>
2023-12-13 18:31:28 -0300

    linters: resolve rubocop offenses on 'user' model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   before_create :set_default_photo
 
-  has_many :posts, class_name: "Post", foreign_key: :author_id
-  has_many :comments, class_name: "Comment", foreign_key: :user_id
-  has_many :likes, class_name: "Like", foreign_key: :user_id
+  has_many :posts, class_name: 'Post', foreign_key: :author_id
+  has_many :comments, class_name: 'Comment', foreign_key: :user_id
+  has_many :likes, class_name: 'Like', foreign_key: :user_id
 
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }

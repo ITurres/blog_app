974c1a29284e5f23096967ef87dfe4fa2296d898
demesameneshoa <demesameneshoa@gmail.com>
2023-12-13 22:47:38 +0300

    feat: added devise attributes for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :posts, class_name: 'Post', foreign_key: :author_id
   has_many :comments, class_name: 'Comment', foreign_key: :user_id
   has_many :likes, class_name: 'Like', foreign_key: :user_id

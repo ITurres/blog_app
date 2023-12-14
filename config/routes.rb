19923721bb4125390320698f5ecaea4ebb792575
ITurres <emanueliturres.co@gmail.com>
2023-12-14 19:08:18 -0300

    Git: resolve conflicts on 'routes.rb' and '_posts.html.erb'

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create destroy] do
-      resources :comments, only: %i[new create]
+      resources :comments, only: %i[new create destroy]
       resources :likes, only: %i[create]
     end
   end

bf76a768743e37c1b64e4b1a28f505569a9e82d6
ITurres <emanueliturres.co@gmail.com>
2023-12-14 18:09:22 -0300

    Feat: add 'destroy' to 'comments-resources'

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
+    resources :posts, only: %i[index show new create destroy] do
       resources :comments, only: %i[new create]
       resources :likes, only: %i[create]
     end

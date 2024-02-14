c5b5633159ebc87173b9342c1dd0d51c7e213dd1
ITurres <emanueliturres.co@gmail.com>
2024-02-14 15:35:10 -0300

    Feat: Add global table prefix name to all tables (for multi rails render db)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.active_record.table_name_prefix = 'blog_app_'
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
